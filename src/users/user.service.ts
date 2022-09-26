61e87388e500c055da3c8e11f959c77874ba3342
Gwnagkyo lee <rhkdqhr08@gmail.com>
2022-09-27 00:13:55 +0900

    feat:  Email verify

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -12,6 +12,7 @@ import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
 import { Verification } from './entities/verification.entity';
 import { VerifyEmailOutput } from './dtos/verify-emai.dto';
 import { UserProfileOutput } from './dtos/user-profile.dto';
+import { MailService } from 'src/mail/mail.service';
 
 @Injectable()
 export class UsersService {
@@ -20,6 +21,7 @@ export class UsersService {
     @InjectRepository(Verification)
     private readonly veritication: Repository<Verification>,
     private readonly jwtService: JwtService,
+    private readonly mailService: MailService,
   ) {}
 
   async createAccount({
@@ -38,7 +40,11 @@ export class UsersService {
       const user = await this.users.save(
         this.users.create({ email, password, role }),
       );
-      await this.veritication.save(this.veritication.create({ user }));
+      const verification = await this.veritication.save(
+        this.veritication.create({ user }),
+      );
+
+      this.mailService.sendVerificationEmail(user.email, verification.code);
       return { ok: true };
     } catch (e) {
       //make error
@@ -115,7 +121,11 @@ export class UsersService {
       if (email) {
         user.email = email;
         user.verified = false;
-        await this.veritication.save(this.veritication.create(user));
+        const verification = await this.veritication.save(
+          this.veritication.create({ user }),
+        );
+
+        this.mailService.sendVerificationEmail(user.email, verification.code);
       }
       if (password) {
         user.password = password;
@@ -141,7 +151,8 @@ export class UsersService {
       });
       if (verification) {
         verification.user.verified = true;
-        this.users.save(verification.user);
+        await this.users.save(verification.user);
+        await this.veritication.delete(verification.id);
         return {
           ok: true,
         };
