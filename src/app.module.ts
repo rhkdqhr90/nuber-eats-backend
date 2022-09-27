91fe5cedd3ad8b518691873324d9172c04f26ef5
Gwnagkyo lee <rhkdqhr08@gmail.com>
2022-09-27 21:17:04 +0900

    feat: update jest

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { MailModule } from './mail/mail.module';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -49,7 +49,8 @@ import { MailModule } from './mail/mail.module';
       database: process.env.DB_NAME,
       entities: [User, Verification],
       synchronize: process.env.NODE_ENV !== 'prod',
-      logging: process.env.NODE_ENV !== 'prod',
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
     }),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
